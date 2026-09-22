945b7439985657b429f5dc49ba463b16dff6ee44
peiyingzhou0104 <peiying.zhou0104@gmail.com>
2026-09-22 16:13:04 +0800

    123

---
+++ w3.js
@@ -0,0 +1,18 @@
+var weight = 60;
+var height = 160;
+
+var readline=require("readline-sync");
+
+var weight=readline.que
+
+var bmi = weight / (height / 100 ** 2);
+console.log("Your BMI:" + bmi); 
+
+function test(weight=70){     
+        var weight=80;
+        var weight=90;
+        console.log("weight="+weight)
+        return weight; 
+         }  
+var result=test();
+console.log("weight="+weight)
